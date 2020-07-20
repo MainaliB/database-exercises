4800caac1490a9abeffae39b22db4a7b00eef61b
Bibek Mainali <bibek.mainali20@gmail.com>
2020-07-20 14:22:28 -0500

    database table exercise

--- table_exercises.sql
+++ table_exercises.sql
@@ -2,7 +2,8 @@ USE employees; # 3
 SHOW tables; # 4
 DESCRIBE employees; #5
 DESCRIBE departments;
-SHOW CREATE TABLE dept_manager;
+# 9 there is no direct relationship between employees and department table, however, they are linked to each other through other table
+SHOW CREATE TABLE dept_manager; # displays the query language structure used to create the table dept_manager
 
 
 SHOW tables;
