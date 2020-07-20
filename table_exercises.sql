a0b318bb3cb7213cbe2c752b7425b9089b4b69bc
Bibek Mainali <bibek.mainali20@gmail.com>
2020-07-20 14:14:08 -0500

    table exercise

---
+++ table_exercises.sql
@@ -0,0 +1,18 @@
+USE employees; # 3
+SHOW tables; # 4
+DESCRIBE employees; #5
+DESCRIBE departments;
+SHOW CREATE TABLE dept_manager;
+
+
+SHOW tables;
+DESCRIBE current_dept_emp;
+DESCRIBE dept_emp_latest_date;
+DESCRIBE departments;
+DESCRIBE dept_emp;
+DESCRIBE dept_manager;
+DESCRIBE employees;
+DESCRIBE employees_with_departments;
+DESCRIBE salaries;
+DESCRIBE titles;
+
