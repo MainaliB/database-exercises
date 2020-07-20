3da68e8e66eb896750f0c499742624d1fbe239c9
Bibek Mainali <bibek.mainali20@gmail.com>
2020-07-20 15:49:02 -0500

    Basic statements practice exercises

---
+++ basic_statements.sql
@@ -0,0 +1,33 @@
+
+#2
+USE albums_db;
+#3
+DESCRIBE albums;
+
+# 4
+SELECT * FROM albums
+WHERE artist = 'Pink Floyd';
+
+select release_date from albums 
+where name = "Sgt. Pepper's Lonely Hearts Club Band";
+
+select genre from albums
+where name = 'Nevermind';
+
+select * 
+from albums
+where release_date between '1990' and '1999';
+
+
+select * 
+from albums
+where sales > '20.0';
+
+
+select * from albums
+where genre = 'Rock';
+
+
+
+
+
