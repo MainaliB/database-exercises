12b0287c914c9ed2ea3d59da1b6e25ba2d95178d
Bibek Mainali <bibek.mainali20@gmail.com>
2020-07-21 16:31:29 -0500

    Limit Exercise Completed

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -1,3 +1,6 @@
+SELECT DISTINCT title
+FROM titles;
+
 # 1
 SELECT DISTINCT last_name
 FROM employees
@@ -19,3 +22,28 @@ ORDER BY birth_date ASC, hire_date DESC
 LIMIT 5 OFFSET 45;
 
 # Page Number = (offset/limit) + 1
+
+
+USE chipotle;
+select * 
+from orders
+where item_price like '$1%'
+order by quantity DESC
+limit 20 offset 50;
+
+USE employees;
+
+SELECT * 
+FROM titles
+WHERE title = "Engineer" AND from_date like '20%%-%%-%%' AND to_date like '99%%-%%-%%'
+ORDER BY emp_no;
+
+SELECT *
+FROM titles
+WHERE title = 'staff'
+ORDER BY emp_no DESC
+LIMIT 20 OFFSET 100;
+
+SELECT * 
+FROM employees
+WHERE title = staff
